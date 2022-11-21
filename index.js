3c8620f685a6fc177e022622f50820399f783a3e
zawyehtoo <zawyehtoo29@gmail.com>
2022-11-21 16:05:59 +0630

    v-1

---
+++ index.js
@@ -0,0 +1,8 @@
+const menuTag=document.querySelector(".menu")
+const menubarTag=document.querySelector(".menubar")
+
+menubarTag.addEventListener("click",()=>{
+    menuTag.classList.toggle("burger")
+})
+
+
